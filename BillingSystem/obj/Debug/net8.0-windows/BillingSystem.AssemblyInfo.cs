2fb57b5660c0f58ed143e1f394765c0b7e68c6a7
Jared Errol B. Villar <jarederrolbasilla.villar25@gmail.com>
2026-08-31 22:00:46 +0800

    Temporary changes before merge

--- BillingSystem/obj/Debug/net8.0-windows/BillingSystem.AssemblyInfo.cs
+++ BillingSystem/obj/Debug/net8.0-windows/BillingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BillingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad7f38f4cc29f769b037df3f9bdcc2b4189e799")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a47b842a13bf77223aa2d2a7ed9e38580687ed87")]
 [assembly: System.Reflection.AssemblyProductAttribute("BillingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BillingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
